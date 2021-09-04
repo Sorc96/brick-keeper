5fc89cb5c6e273e00c18c3010fa467d5e1ce7a58
Matous Vokal <vokalmat@fit.cvut.cz>
2021-09-04 19:42:03 +0200

    Add Javascript for parsing Bricklink orders

---
+++ parse_order.js
@@ -0,0 +1,27 @@
+function parseRow(row) {
+  const cells = row.querySelectorAll('td')
+  const [color, partType] = parsePart(cells[2])
+  const quantity = parseQuantity(cells[4])
+  const unitprice = parseUnitprice(cells[5])
+  
+  return [color, partType, quantity, unitprice]
+}
+
+function parsePart(cell) {
+  const partString = cell.querySelector('font').innerHTML
+  const relevantPart = partString.split('</div>&nbsp;')[1]
+  return relevantPart.split('<br>').map(s => s.trim())
+}
+
+function parseQuantity(cell) {
+  return cell.textContent
+}
+
+function parseUnitprice(cell) {
+  return cell.textContent.split(/\s/)[1]
+}
+
+const rows = [...document.querySelectorAll('tr.avoidBreak')]
+const parsedRows = rows.map(parseRow)
+const resultJson = JSON.stringify(parsedRows)
+console.log(resultJson)
