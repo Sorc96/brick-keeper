df8c8dc953da823d53619fecf549144a9d291199
Matous Vokal <vokalmat@fit.cvut.cz>
2021-11-12 20:46:30 +0100

    Simplify item repo query syntax

--- lib/repos/item_repo.rb
+++ lib/repos/item_repo.rb
@@ -8,7 +8,7 @@ module Repos
       parts
         .join(:colors)
         .join(:part_types)
-        .where { { colors[:name] => color, part_types[:name] => type } }
+        .where(colors[:name] => color, part_types[:name] => type)
         .select(:id, colors[:name].as(:color), part_types[:name].as(:part_type))
         .combine(:lots)
         .map_with(:items_mapper)
